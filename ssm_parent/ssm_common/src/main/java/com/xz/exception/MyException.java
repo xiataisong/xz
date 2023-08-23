470ee8ff5d3e58e5d6b32b5fe706d1296bdfa46c
xz <2896506735@qq.com>
2023-08-23 08:59:28 +0800

    添加注释

--- ssm_parent/ssm_common/src/main/java/com/xz/exception/MyException.java
+++ ssm_parent/ssm_common/src/main/java/com/xz/exception/MyException.java
@@ -14,7 +14,7 @@ public class MyException extends RuntimeException {
      * 构建 就要传入提示的信息
      * @param message
      */
-    public MyException(String message){
+    public MyException(String message){     //自定义异常
         super(message);
     }
 }
